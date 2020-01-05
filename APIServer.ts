13cd95a5e488369b5ca1f9d4f37438b279080ae9
Jeffery Cosby <jcoz1968@gmail.com>
2020-01-04 18:36:03 -0600

    setup for chapter 3

--- APIServer.ts
+++ APIServer.ts
@@ -1,50 +1,112 @@
 import * as http from "http";
-import express, { Request, Response, Express } from "express";
+import express, { Express } from "express";
 import bodyParser from "body-parser";
+import EntityRouter from './EntityRouter';
+import BaseEntity from "./entities/BaseEntity";
 
 export default class APIServer {
-  private _app: Express;
 
-  get app(): Express {
-    return this._app;
-  }
+    private _app: Express;
 
-  private _server: http.Server;
+    get app(): Express {
+        return this._app;
+    }
 
-  get server(): http.Server {
-    return this._server;
-  }
+    private _server: http.Server;
 
-  constructor() {
-    this._app = express();
-    this._app.set("port", process.env.PORT || 3000);
+    get server(): http.Server {
+        return this._server;
+    }
 
-    this.configureMiddleware();
-  }
+    constructor() {
+        this._app = express();
 
-  public configureMiddleware() {
-    this._app.use(bodyParser.json());
-    this._app.use(bodyParser.urlencoded({ extended: true }));
+        // Set port
+        this._app.set("port", process.env.PORT || 3000);
 
-    this.app.use(function(req, res, next) {
-      res.setHeader("Access-Control-Allow-Origin", "*");
-      res.setHeader("Access-Control-Allow-Credentials", "true");
-      res.setHeader(
-        "Access-Control-Allow-Methods",
-        "GET,HEAD,OPTIONS,POST,PUT"
-      );
-      res.setHeader(
-        "Access-Control-Allow-Headers",
-        "Access-Control-Allow-Origin,Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,Authorization"
-      );
-      next();
-    });
-  }
+        // Add Middleware
+        this.configureMiddleware();
+    }
 
-  public start() {
-    // Start the server instance
-    this._server = this._app.listen(this._app.get("port"), () => {
-      console.log("Server is running on port " + this._app.get("port"));
-    });
-  }
+    public configureMiddleware() {
+        // Setup body parsing - required for POST requests
+        this._app.use(bodyParser.json());
+        this._app.use(bodyParser.urlencoded({ extended: true }));
+
+        // Setup CORS
+        this.app.use(function (req, res, next) {
+            res.setHeader("Access-Control-Allow-Origin", "*");
+            res.setHeader("Access-Control-Allow-Credentials", "true");
+            res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+            res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Origin,Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,Authorization");
+            next();
+        });
+    }
+
+    public addEntity<T extends BaseEntity>(clazz) {
+        const name = Reflect.getMetadata("entity:name", clazz);
+        let entityRouter = new EntityRouter<T>(name, clazz);
+        this._app.use(`/${name}`, entityRouter.router);
+    }
+
+    public start() {
+        // Start the server instance
+        this._server = this._app.listen(this._app.get("port"), () => {
+            console.log("Server is running on port " + this._app.get("port"));
+        });
+    }
+
 }
+
+
+
+// import * as http from "http";
+// import express, { Request, Response, Express } from "express";
+// import bodyParser from "body-parser";
+
+// export default class APIServer {
+//   private _app: Express;
+
+//   get app(): Express {
+//     return this._app;
+//   }
+
+//   private _server: http.Server;
+
+//   get server(): http.Server {
+//     return this._server;
+//   }
+
+//   constructor() {
+//     this._app = express();
+//     this._app.set("port", process.env.PORT || 3000);
+
+//     this.configureMiddleware();
+//   }
+
+//   public configureMiddleware() {
+//     this._app.use(bodyParser.json());
+//     this._app.use(bodyParser.urlencoded({ extended: true }));
+
+//     this.app.use(function(req, res, next) {
+//       res.setHeader("Access-Control-Allow-Origin", "*");
+//       res.setHeader("Access-Control-Allow-Credentials", "true");
+//       res.setHeader(
+//         "Access-Control-Allow-Methods",
+//         "GET,HEAD,OPTIONS,POST,PUT"
+//       );
+//       res.setHeader(
+//         "Access-Control-Allow-Headers",
+//         "Access-Control-Allow-Origin,Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,Authorization"
+//       );
+//       next();
+//     });
+//   }
+
+//   public start() {
+//     // Start the server instance
+//     this._server = this._app.listen(this._app.get("port"), () => {
+//       console.log("Server is running on port " + this._app.get("port"));
+//     });
+//   }
+// }
--- app.ts
+++ app.ts
@@ -1,70 +1,83 @@
-import APIServer from './APIServer';
-import { Request, Response, Router } from 'express';
+import { JsonDB } from 'node-json-db';
+import { Config } from 'node-json-db/dist/lib/JsonDBConfig'
+import APIServer from "./APIServer";
+import Person from './entities/Person';
 
-const server = new APIServer();
+export const apiServer = new APIServer();
+export const db = new JsonDB(new Config("entityDatabase", true, true, '/'));
 
-class APIRoutes {
+apiServer.addEntity<Person>(Person);
 
-  @logRoute()
-  @route('get', '/')
-  public indexRoute(req: Request, res: Response) {
-    return {
-      'Hello': 'World'
-    };
-  }
+apiServer.start();
 
-  @logRoute()
-  @route('get', '/people')
-  @authenticate('123456')
-  public peopleRoute(req: Request, res: Response) {
-    return {
-      people: [
-        {
-          'firstName': 'Jeffery',
-          'lastName': 'Cosby'
-        },
-        {
-          'firstName': 'Brenna',
-          'lastName': 'Cosby'
-        }        
-      ]
-    };
-  }
 
-}
+// import APIServer from './APIServer';
+// import { Request, Response, Router } from 'express';
 
-function route(method: string, path: string): MethodDecorator {
-  return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
-    server.app[method](path, (req: Request, res: Response) => {
-      res.status(200).json(descriptor.value(req, res));
-    });
-  }
-}
+// const server = new APIServer();
 
-function logRoute(): MethodDecorator {
-  return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
-    const original = descriptor.value;
-    descriptor.value = function (...args: any[]) {
-      let req = args[0] as Request;
-      console.log(`${req.url} ${req.method} Called`);
-      return original.apply(this, args);
-    }
-  }
-}
+// class APIRoutes {
 
-function authenticate(key: string): MethodDecorator {
-  return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
-    const original = descriptor.value;
-    descriptor.value = function (...args: any[]) {
-      const req = args[0] as Request;
-      const res = args[1] as Response;
-      const headers = req.headers;      
-      if (headers.hasOwnProperty('apiKey') && headers.apiKey === key) {
-        return original.apply(this, args);
-      }
-      res.status(403).json({ error: 'Not Auithorized' });
-    }
-  }
-}
+//   @logRoute()
+//   @route('get', '/')
+//   public indexRoute(req: Request, res: Response) {
+//     return {
+//       'Hello': 'World'
+//     };
+//   }
 
-server.start();
+//   @logRoute()
+//   @route('get', '/people')
+//   @authenticate('123456')
+//   public peopleRoute(req: Request, res: Response) {
+//     return {
+//       people: [
+//         {
+//           'firstName': 'Jeffery',
+//           'lastName': 'Cosby'
+//         },
+//         {
+//           'firstName': 'Brenna',
+//           'lastName': 'Cosby'
+//         }        
+//       ]
+//     };
+//   }
+
+// }
+
+// function route(method: string, path: string): MethodDecorator {
+//   return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+//     server.app[method](path, (req: Request, res: Response) => {
+//       res.status(200).json(descriptor.value(req, res));
+//     });
+//   }
+// }
+
+// function logRoute(): MethodDecorator {
+//   return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+//     const original = descriptor.value;
+//     descriptor.value = function (...args: any[]) {
+//       let req = args[0] as Request;
+//       console.log(`${req.url} ${req.method} Called`);
+//       return original.apply(this, args);
+//     }
+//   }
+// }
+
+// function authenticate(key: string): MethodDecorator {
+//   return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+//     const original = descriptor.value;
+//     descriptor.value = function (...args: any[]) {
+//       const req = args[0] as Request;
+//       const res = args[1] as Response;
+//       const headers = req.headers;      
+//       if (headers.hasOwnProperty('apiKey') && headers.apiKey === key) {
+//         return original.apply(this, args);
+//       }
+//       res.status(403).json({ error: 'Not Auithorized' });
+//     }
+//   }
+// }
+
+// server.start();
